7dbf11c0e5909aa1d313aab620626d5649ab8910
jonatas araujo <jonatasaraujopf@icloud.com>
2025-06-12 08:32:15 -0300

    Initial Commit

---
+++ Caption Buddy/Caption_BuddyApp.swift
@@ -0,0 +1,20 @@
+//
+//  Caption_BuddyApp.swift
+//  Caption Buddy
+//
+//  Created by Jonatas Araujo on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Caption_BuddyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
